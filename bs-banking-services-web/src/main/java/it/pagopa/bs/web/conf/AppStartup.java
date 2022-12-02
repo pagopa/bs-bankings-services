ac1df0449643f305af991c95a34132eeef1c4682
denomis <simone.diotti.esterno@pagopa.it>
2022-12-02 17:01:26 +0100

    added basic properties and startup class

---
+++ bs-banking-services-web/src/main/java/it/pagopa/bs/web/conf/AppStartup.java
@@ -0,0 +1,12 @@
+package it.pagopa.bs.web.conf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStartup {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppStartup.class, args);
+    }
+}
