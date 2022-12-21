1fe85e5e879925cecaff154faa583d61b7867345
denomis <simone.diotti.esterno@pagopa.it>
2022-12-21 17:27:14 +0100

    added test mapper in check iban controller

---
+++ bs-banking-services-web/src/main/java/it/pagopa/bs/web/controller/checkiban/CheckIbanController.java
@@ -0,0 +1,28 @@
+package it.pagopa.bs.web.controller.checkiban;
+
+import lombok.CustomLog;
+import lombok.RequiredArgsConstructor;
+import lombok.var;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.pagopa.bs.web.mapper.DualMapper;
+import reactor.core.publisher.Mono;
+
+@RestController
+@CustomLog
+@RequiredArgsConstructor
+@RequestMapping("${pagopa.bs.api-version-path}")
+public class CheckIbanController {
+
+    private final DualMapper duabDualMapper;
+
+    @PostMapping("/validate-account-holder")
+    public Mono<Void> validateAccountHolder() {
+        log.info("test");
+        var a = duabDualMapper.getLiveness();
+        return Mono.empty();
+    }
+}
