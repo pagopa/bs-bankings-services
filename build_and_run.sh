f36809918c955ed4d671ecdfb0e6a18a5fc6fc1d
denomis <simone.diotti.esterno@pagopa.it>
2023-01-05 11:49:08 +0100

    deleted old start/stop scripts

--- build_and_run.sh
+++
@@ -1,3 +0,0 @@
-export IMAGE_NAME="pagopa/bs-banking-services"
-docker build -t $IMAGE_NAME .
-docker container run -d --name bs --rm -p 8080:8080 -p 5005:5005 $IMAGE_NAME
--- stop_and_delete.sh
+++
@@ -1,3 +0,0 @@
-export IMAGE_NAME="pagopa/bs-banking-services"
-docker stop bs
-docker rmi $IMAGE_NAME
