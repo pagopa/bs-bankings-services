4f9dbc1808b1ec0f5f359a099f07aa40c9526266
denomis <simone.diotti.esterno@pagopa.it>
2023-01-11 16:25:47 +0100

    deleted duplicate class

--- bs-banking-services-checkiban/src/main/java/it/pagopa/bs/checkiban/enumeration/RegexPatterns.java
+++
@@ -1,8 +0,0 @@
-package it.pagopa.bs.checkiban.enumeration;
-
-public class RegexPatterns {
-
-    private RegexPatterns() {}
-
-    public static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
-}
