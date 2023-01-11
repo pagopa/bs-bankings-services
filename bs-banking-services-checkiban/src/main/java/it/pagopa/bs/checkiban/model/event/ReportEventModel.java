fd8ad7741c186d0bdde647ad10f622efcb0f0914
denomis <simone.diotti.esterno@pagopa.it>
2023-01-11 16:40:20 +0100

    added models for event reporting

---
+++ bs-banking-services-checkiban/src/main/java/it/pagopa/bs/checkiban/model/event/ReportEventModel.java
@@ -0,0 +1,40 @@
+package it.pagopa.bs.checkiban.model.event;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import it.pagopa.bs.checkiban.enumeration.ServiceCode;
+import it.pagopa.bs.common.deserializer.LocalDateDeserializer;
+import it.pagopa.bs.common.serializer.LocalDateSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReportEventModel implements Serializable {
+
+    private String eventUid;
+    private String bulkRequestId;
+    private String requestCode;
+    private String correlationId;
+    private String subscriptionId;
+    private String credentialId;
+    private Long modelVersion;
+    private ServiceCode serviceCode;
+    private String destination;
+    private String obo;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDate referenceDate;
+
+    private JsonNode payload;
+}
