741c5e33e0b5e14852a08da62590935827c0052c
denomis <simone.diotti.esterno@pagopa.it>
2023-01-23 15:19:50 +0100

    fixed db config and dependencies

--- bs-banking-services-web/src/main/java/it/pagopa/bs/web/conf/MyBatisConfig.java
+++ bs-banking-services-web/src/main/java/it/pagopa/bs/web/conf/MyBatisConfig.java
@@ -1,13 +1,6 @@
 package it.pagopa.bs.web.conf;
 
-import javax.sql.DataSource;
-
-import org.springframework.core.io.Resource;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -19,16 +12,4 @@ import lombok.RequiredArgsConstructor;
 @MapperScan("it.pagopa.bs.web.mapper")
 public class MyBatisConfig {
 
-    private final DataSource dataSource;
-
-    @Value("classpath*:it/pagopa/**/mapper/**/*.xml")
-    private Resource[] mapperResources;
-    
-    @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
-        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setMapperLocations(mapperResources);
-        return factoryBean.getObject();
-    }
 }
