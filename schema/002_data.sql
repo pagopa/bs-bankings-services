91e2a626c53dd2e9a5420700a5f5c4cb85d8e05b
denomis <simone.diotti.esterno@pagopa.it>
2023-02-03 16:15:35 +0100

    added bulk connector to sql init scripts

--- schema/002_data.sql
+++ schema/002_data.sql
@@ -18,6 +18,7 @@ COMMIT;
 INSERT INTO pagopa_ma_psp VALUES(1, '00000', 'IT', 'MOCKITMBXXX', 'IBAN', 0, null);
 INSERT INTO pagopa_ma_psp VALUES(2, '00001', 'IT', 'MOCKITMCXXX', 'IBAN', 0, null);
 INSERT INTO pagopa_ma_psp VALUES(3, '00002', 'IT', 'MOCKITMDXXX', 'IBAN', 0, null);
+INSERT INTO pagopa_ma_psp VALUES(4, '00003', 'IT', 'MOCKITMEXXX', 'IBAN', 0, null);
 COMMIT;
 
 INSERT INTO pagopa_ma_south_config VALUES(PAGOPA_SQ_SCF_ID.nextval, 'PSP_MOCK_API', 'Mock Bank Connector', 'MOCK', 'PSP_API_STANDARD', 1, '{"southPath":"/api/pagopa/banking/v4.0/utils/validate-account-holder/south/mock"}', 0, FROM_TZ(TIMESTAMP '2020-06-18 07:00:00', 'Europe/Berlin'), FROM_TZ(TIMESTAMP '2020-06-18 07:00:00', 'Europe/Berlin'), NULL);
@@ -25,6 +26,9 @@ INSERT INTO pagopa_ma_south_config VALUES(PAGOPA_SQ_SCF_ID.nextval, 'PSP_STELLA_
 
 INSERT INTO pagopa_ma_south_config VALUES(PAGOPA_SQ_SCF_ID.nextval, 'SERVICE_PROVIDER_FAST_MOCK_API', 'Fast Mock Service Provider Connector', 'FAST_MOCK', 'SERVICE_PROVIDER_API_STANDARD', 1, '{"southPath":null,"hasGenericSearch":true,"isMock":true,"isPrivative":false,"isActive":true}', 0, FROM_TZ(TIMESTAMP '2020-06-18 07:00:00', 'Europe/Berlin'), FROM_TZ(TIMESTAMP '2020-06-18 07:00:00', 'Europe/Berlin'), NULL);
 INSERT INTO pagopa_ma_south_config VALUES(PAGOPA_SQ_SCF_ID.nextval, 'SERVICE_PROVIDER_SLOW_MOCK_API', 'Slow Mock Service Provider Connector', 'SLOW_MOCK', 'SERVICE_PROVIDER_API_STANDARD', 1, '{"southPath":null,"hasGenericSearch":true,"isMock":true,"isPrivative":false,"isActive":true}', 0, FROM_TZ(TIMESTAMP '2020-06-18 07:00:00', 'Europe/Berlin'), FROM_TZ(TIMESTAMP '2020-06-18 07:00:00', 'Europe/Berlin'), NULL);
+
+INSERT INTO pagopa_ma_south_config VALUES(PAGOPA_SQ_SCF_ID.nextval, 'TEST_BATCH', 'Test Batch Connector', 'BATCH_TEST', 'PSP_BATCH_STANDARD', 1, '{"maxRecords":50000,"writeCutoffTime":"17:00:00","readCutoffTime":"05:30:00"}', 0, FROM_TZ(TIMESTAMP '2020-06-18 07:00:00', 'Europe/Berlin'), FROM_TZ(TIMESTAMP '2020-06-18 07:00:00', 'Europe/Berlin'), NULL);
+
 COMMIT;
 
 INSERT INTO pagopa_ma_service_binding VALUES(PAGOPA_SQ_SBI_ID.nextval, FROM_TZ(TIMESTAMP '2020-06-17 07:00:01', 'Europe/Berlin'), null, 1, 1, 1);
@@ -35,4 +39,5 @@ INSERT INTO pagopa_ma_service_binding VALUES(PAGOPA_SQ_SBI_ID.nextval, FROM_TZ(T
 INSERT INTO pagopa_ma_service_binding VALUES(PAGOPA_SQ_SBI_ID.nextval, FROM_TZ(TIMESTAMP '2021-02-18 07:00:01', 'Europe/Berlin'), null, 2, 3, 2);
 INSERT INTO pagopa_ma_service_binding VALUES(PAGOPA_SQ_SBI_ID.nextval, FROM_TZ(TIMESTAMP '2021-02-18 07:00:01', 'Europe/Berlin'), null, 3, 4, 2);
 
+INSERT INTO pagopa_ma_service_binding VALUES(PAGOPA_SQ_SBI_ID.nextval, FROM_TZ(TIMESTAMP '2021-02-18 07:00:01', 'Europe/Berlin'), null, 4, 5, 2);
 COMMIT;
