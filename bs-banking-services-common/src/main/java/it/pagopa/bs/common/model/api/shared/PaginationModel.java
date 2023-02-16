f243eb7f2190c9449ad7a27953e5303a9df6ef51
denomis <simone.diotti.esterno@pagopa.it>
2023-02-16 17:18:57 +0100

    fixed pagination model

--- bs-banking-services-common/src/main/java/it/pagopa/bs/common/model/api/shared/PaginationModel.java
+++ bs-banking-services-common/src/main/java/it/pagopa/bs/common/model/api/shared/PaginationModel.java
@@ -1,17 +1,20 @@
 package it.pagopa.bs.common.model.api.shared;
 
 import java.io.Serializable;
+
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 
 import lombok.Data;
 
 @Data
+@Valid
 public class PaginationModel implements Serializable {
 
     private long pageCount;
     private long resultCount;
     private @Min(0L) long offset;
-    private @Min(0L) long limit;
+    private @Min(10L) long limit;
 
     public PaginationModel() {}
 
